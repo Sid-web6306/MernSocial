16588a9933c2d528520abe2fba91de45f1ee78cd
Pranjal <pranjaljsl2910@gmail.com>
2020-11-10 17:38:10 +0530

    routes and controller web created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
+// const express = require('express');
+
+// const router = express.Router();
+// const homeController =require('../controllers/home_controller_action');
+// console.log('router loadeddddddddddddddddddddddddddddddd');
+
+// router.get('/',homeController.home);
+// router.use('/user',require('./users'));
+// module.exports = router;
+
 const express = require('express');
 
 const router = express.Router();
-const homeController =require('../controllers/home_controller_action');
-console.log('router loadeddddddddddddddddddddddddddddddd');
+const homeController = require('../controllers/home_controller_action');
 
-router.get('/',homeController.home);
+console.log('router loaded');
+
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+// for anty further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
 
 module.exports = router;
